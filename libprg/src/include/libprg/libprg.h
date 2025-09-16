05ac98fd0b822d317664354d40d64420bf18ed7b
aluno <Maria.fc2005@aluno.ifsc.edu.br>
2025-09-16 09:22:21 -0300

    feat: atualizando a pilha

--- libprg/src/include/libprg/libprg.h
+++ libprg/src/include/libprg/libprg.h
@@ -8,16 +8,28 @@ typedef struct {
     int error;
 } result_t;
 
+typedef struct pilha pilha_t;
+
+pilha_t* criar_pilha(int capacidade);
+void empilhar(pilha_t* pilha, int valor);
+int desempilhar(pilha_t* pilha);
+int tamanho(pilha_t* pilha);
+int vazia(pilha_t* pilha);
+void destruir(pilha_t* pilha);
+
+/*-----------------------------------------*/
+
 typedef enum { SUM, SUB } operation_t;
 
 result_t sub(double a, double b);
 result_t sum(double a, double b);
 result_t compute(double a, double b, operation_t op);
 
-#endif
-
 /*----FILA----*/
 
 typedef struct fila fila_t;
 fila_t* criar_fila(int capacidade) ;
 void enfileirar(fila_t* fila, int valor);
+
+
+#endif
