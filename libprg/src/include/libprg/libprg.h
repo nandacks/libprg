ab27155061ecee8e9e2aa965613a990ea1df1e9b
aluno <Maria.fc2005@aluno.ifsc.edu.br>
2025-09-25 11:27:34 -0300

    feat: arrumando a pilha

--- libprg/src/include/libprg/libprg.h
+++ libprg/src/include/libprg/libprg.h
@@ -11,33 +11,33 @@ typedef struct {
 
 typedef struct pilha pilha_t;
 
-pilha_t* criar_pilha(int capacidade);
-void empilhar(pilha_t* pilha, int valor);
+pilha_t* criar_pilha(pilha_t* p1);
+void empilhar(pilha_t* pilha);
 int desempilhar(pilha_t* pilha);
 int tamanho(pilha_t* pilha);
 int vazia(pilha_t* pilha);
 void destruir(pilha_t* pilha);
 
-/*-----------------------------------------*/
-
-typedef enum { SUM, SUB } operation_t;
-
-result_t sub(double a, double b);
-result_t sum(double a, double b);
-result_t compute(double a, double b, operation_t op);
-
-/*----FILA----*/
-
-typedef struct fila fila_t;
-fila_t* criar_fila(int capacidade) ;
-void enfileirar(fila_t* fila, int valor);
-
-/*----LISTA----*/
-
-typedef struct lista_linear lista_linear_t;
-lista_linear_t* criar_lista_linear(int capacidade, bool ordenada);
-void inserir_lista_linear(lista_linear_t* lista, int valor);
-bool cheia_lista_linear(lista_linear_t* lista);
-int buscar_lista(lista_linear_t* lista, int valor);
-
-#endif
+// /*-----------------------------------------*/
+//
+// typedef enum { SUM, SUB } operation_t;
+//
+// result_t sub(double a, double b);
+// result_t sum(double a, double b);
+// result_t compute(double a, double b, operation_t op);
+//
+// /*----FILA----*/
+//
+// typedef struct fila fila_t;
+// fila_t* criar_fila(int capacidade) ;
+// void enfileirar(fila_t* fila, int valor);
+//
+// /*----LISTA----*/
+//
+// typedef struct lista_linear lista_linear_t;
+// lista_linear_t* criar_lista_linear(int capacidade, bool ordenada);
+// void inserir_lista_linear(lista_linear_t* lista, int valor);
+// bool cheia_lista_linear(lista_linear_t* lista);
+// int buscar_lista(lista_linear_t* lista, int valor);
+//
+// #endif
