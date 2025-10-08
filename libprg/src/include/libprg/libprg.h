c092de5f1a63424130acb918d3d38cb785d3077f
Maria Fernanda da Cunha <Maria.fc2005@aluno.ifsc.edu.br>
2025-10-08 08:19:21 -0300

    feat: atualizando dados da pilha.c e de libprg.h

--- libprg/src/include/libprg/libprg.h
+++ libprg/src/include/libprg/libprg.h
@@ -2,21 +2,32 @@
 #define LIBPRG_LIBPRG_H
 #include <stdbool.h>
 
+/*----RESULTADOS----*/
+
+typedef struct {
+    double value;
+    int error;
+} result_t;
+
+typedef enum { SUM, SUB } operation_t;
+
+result_t sub(double a, double b);
+
+result_t sum(double a, double b);
+
+result_t compute(double a, double b, operation_t op);
+
 /*----PILHA----*/
 
-// typedef struct {
-//     double value;
-//     int error;
-// } result_t;
-//
-// typedef struct pilha pilha_t;
-//
-// pilha_t* criar_pilha(pilha_t* p1);
-// void empilhar(pilha_t* pilha);
-// int desempilhar(pilha_t* pilha);
-// int tamanho(pilha_t* pilha);
-// int vazia(pilha_t* pilha);
-// void destruir(pilha_t* pilha);
+typedef struct pilha pilha_t;
+
+pilha_t* criar_pilha(pilha_t* pilha, int capacidade);
+int pilha_cheia(pilha_t* pilha);
+int empilhar(pilha_t* pilha, int valor);
+int desempilha(pilha_t* pilha);
+int tamanho(pilha_t* pilha);
+void destruir(pilha_t* pilha);
+int topo(pilha_t* pilha);
 
 // /*-----------------------------------------*/
 //
@@ -28,16 +39,16 @@
 //
 // /*----FILA----*/
 
-typedef struct fila fila_t;
-fila_t* criar_fila(int capacidade);
-void destruir_fila(fila_t* fila);
-bool vazia(fila_t* fila);
-bool cheia(fila_t* fila);
-int tamanho(fila_t* fila);
-bool enfileirar(fila_t* fila, int valor);
-bool desenfileirar(fila_t* fila, int* valor_removido);
-bool ver_inicio(fila_t* fila, int* valor_inicio);
-bool ver_fim(fila_t* fila, int* valor_fim);
+// typedef struct fila fila_t;
+// fila_t* criar_fila(int capacidade);
+// void destruir_fila(fila_t* fila);
+// bool vazia(fila_t* fila);
+// bool cheia(fila_t* fila);
+// int tamanho(fila_t* fila);
+// bool enfileirar(fila_t* fila, int valor);
+// bool desenfileirar(fila_t* fila, int* valor_removido);
+// bool ver_inicio(fila_t* fila, int* valor_inicio);
+// bool ver_fim(fila_t* fila, int* valor_fim);
 
 // /*----LISTA----*/
 //
@@ -47,4 +58,22 @@ bool ver_fim(fila_t* fila, int* valor_fim);
 // bool cheia_lista_linear(lista_linear_t* lista);
 // int buscar_lista(lista_linear_t* lista, int valor);
 //
-// #endif
+
+
+// ---------- Lista Ordenada ----------
+//
+// typedef struct lista_ordenada lista_ordenada_t;
+//
+//
+//
+//
+//
+// // ---------- Lista Encadeada ----------
+//
+// typedef struct no no_t;
+//
+// no_t* crar_lista_encadeada(int valor);
+
+
+
+#endif
--- libprg/src/libprg/pilha.c
+++ libprg/src/libprg/pilha.c
@@ -1,5 +1,9 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+#define SIM 1
+#define NAO 2
 
 typedef struct pilha {
     int* elementos;
@@ -8,86 +12,87 @@ typedef struct pilha {
 } pilha_t;
 
 
-pilha_t* criar_pilha() {
-    int capacidade;
-    printf("Qual será a capacidade da sua pilha: ");
-    scanf("%d", &capacidade);
-    pilha_t* p = malloc(sizeof(pilha_t));
+
+pilha_t* criar_pilha(pilha_t* pilha, int capacidade) {
+    pilha_t* p = malloc (sizeof(pilha_t));
     p->elementos = malloc(capacidade * sizeof(int));
-
     p->topo = -1;
-    p->capacidade = capacidade;
+        return 1; // pilha n ta cheia
 
-    return p;
 }
 
-int tamanho(pilha_t* pilha) {
-    return pilha->topo + 1;
+int pilha_cheia(pilha_t* pilha) {
+    if (pilha->topo == pilha->capacidade - 1){
+        printf("Sua pilha já está cheia :( \n");
+        printf("Quer alocar mais espaço na memoria para colocar um novo elemento?\n");
+        printf("Digite %d para SIM ou %d para NAO: ", SIM, NAO);
+        int opcao;
+        scanf("%d", &opcao);
+        switch (opcao) {
+            case SIM:
+                printf("Certo, vamos alocar memoria!\n");
+                pilha->capacidade *= 2;
+                int* novo = realloc(pilha->elementos, pilha->capacidade * sizeof(int));
+                //vendo se teve erro ao por mais memoria
+                if (novo == NULL) {
+                    printf("Erro ao alocar memoria!\n");
+                    return 0; // falhou
+                }
+                pilha->elementos = novo;
+                printf("Pilha expandida! Nova capacidade: %d\n", pilha->capacidade);
+                return 1; // caeto
+
+            case NAO:
+                printf("Você optou por não aumentar a pilha.\n");
+                return 0; // recusou
+
+            default:
+                printf("Opção inválida.\n");
+                return 0;
+        }
+    }
+    return 1; // Pilha não está cheia
 }
 
-int vazia(pilha_t* pilha) {
-    return pilha->topo == -1;
-}
-
-int valor;
-void empilhar(pilha_t* pilha) {
-    int resposta;
-    do {
-        printf("Qual valor deseja inserir na pilha? ");
-        scanf("%d", &valor);
-        if (pilha->topo == (pilha->capacidade - 1)) {
-            printf("\n--- ATENCAO: Pilha cheia! Dobrando a capacidade... ---\n");
-            int* temp = realloc(pilha->elementos, (pilha->capacidade * 2) * sizeof(int));
-            pilha->elementos = temp;
-            pilha->capacidade *= 2;
-            printf("--- Capacidade da pilha aumentada para %d ---\n", pilha->capacidade);
+    int empilhar(pilha_t* pilha, int valor) {
+        if (pilha->topo == pilha->capacidade - 1) {
+            if (!pilha_cheia(pilha)) {
+                printf("Não foi possível empilhar. Pilha cheia!\n");
+                return 0; // deu erro ao emplhar
+            }
         }
         pilha->topo++;
         pilha->elementos[pilha->topo] = valor;
-        printf("=> Valor %d empilhado com sucesso. Tamanho atual: %d\n", valor, tamanho(pilha));
-        int continuar = 1;
-        while (continuar) {
-            resposta = 0;
-            printf("\nDeseja inserir outro item? (1 para SIM / 0 para NAO): ");
-            scanf("%d", &resposta);
-            switch (resposta) {
-                case 1:
-                    empilhar(pilha);
-                    break;
-                case 0:
-                    continuar = 0;
-                    return;
-
-                default:
-                    printf("Opção Invalida, tente novamente! \n");
-                    if (continuar==1) {
-                        if (resposta != 1 || resposta != 0) {
-                            printf("realmente não deseja continuar????? 1/sim 0/não");
-                            scanf("%d", &resposta);
-                        }
-
-            }
-            }
-            printf("\n");
-        }
-    } while (resposta == 1);
-    printf("Operacao de insercao finalizada.\n");
-}
-
-int desempilhar(pilha_t* pilha) {
-    if (vazia(pilha)) {
-        printf("Erro: Pilha vazia!\n");
-        return -1; // Valor de erro
+        printf("Valor %d empilhado com sucesso!\n", valor);
+        return 1;
     }
-    printf("Desempilhado o item %d de valor %d. \n", tamanho(pilha), valor);
-    int valor = pilha->elementos[pilha->topo];
-    pilha->topo--;
-    printf("Agora sua pilha agora tem tamanho %d",tamanho(pilha));
-    return valor;
-}
 
-void destruir(pilha_t* pilha) {
-    free(pilha->elementos);
-    free(pilha);
-    printf("A pilha foi destruida com sucesso!");
+    int desempilha(pilha_t* pilha) {
+        if (pilha->topo == -1) {
+            printf("Pilha vazia, não é possível desempilhar.\n");
+            return -1;
+        }
+        int valor = pilha->elementos[pilha->topo];
+        pilha->topo--;
+        return valor;
+    }
+
+
+    int tamanho(pilha_t* pilha) {
+        return pilha->topo + 1;
+    }
+
+    void destruir(pilha_t* pilha) {
+        if (pilha != NULL) {
+            if (pilha->elementos != NULL) {
+                free(pilha->elementos);
+            }
+            free(pilha);
+        }
+    }
+
+
+    int topo(pilha_t* pilha) {
+    printf("Ultimo elemento da pilha é: %d", pilha->elementos[pilha->topo]);
+    return 0;
 }
